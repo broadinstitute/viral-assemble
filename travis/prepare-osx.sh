daa59e75568e2c13f935eebeb13d20d6908f353f
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-03-17 19:31:08 -0400

    add python virtualenv for osx

--- travis/prepare-osx.sh
+++ travis/prepare-osx.sh
@@ -3,7 +3,7 @@ set -e
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
     brew update
-    #brew install python3
+    brew install pyenv-virtualenv
 
     # Install some custom requirements on OS X
        # e.g. brew install pyenv-virtualenv
@@ -12,10 +12,30 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
             py27)
                 # Install some custom Python 2.7 requirements on OS X
                 brew install python
+                if [ ! -e $HOME/virtualenv/bin/activate ]; then
+                    cd $HOME
+                    virtualenv virtualenv
+                fi
                 ;;
             py35)
                 # Install some custom Python 3.5 requirements on OS X
                 brew install python3
+                if [ ! -e $HOME/virtualenv/bin/activate ]; then
+                    cd $HOME
+                    pyvenv-3.5 virtualenv
+                fi
                 ;;
         esac
+
+
+fi
+
+# if we're on OSX we have to activat the virtualenv ourselves
+# at least until Travis supports Python builds nativelt
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
+    # if the virtual env exists but is not active
+    if [ -e "$HOME/virtualenv/bin/activate" ] && [ -z "$VIRTUAL_ENV" ]; then
+        # activate the virtualenv
+        source $HOME/virtualenv/bin/activate
+    fi
 fi
--- travis/tests-long.sh
+++ travis/tests-long.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+# if we're on OSX we have to activat the virtualenv ourselves
+# at least until Travis supports Python builds nativelt
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
+    # if the virtual env exists but is not active
+    if [ -e "$HOME/virtualenv/bin/activate" ] && [ -z "$VIRTUAL_ENV" ]; then
+        # activate the virtualenv
+        source $HOME/virtualenv/bin/activate
+    fi
+fi
+
 echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 
