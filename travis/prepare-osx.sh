768aca5b53efd9141cfe67955b90e01ef5a50045
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-03-18 11:07:19 -0400

    rely on gcc48 travis supposedly pre-installs

--- travis/prepare-osx.sh
+++ travis/prepare-osx.sh
@@ -4,10 +4,10 @@ set -e
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
     brew update > /dev/null
     brew tap homebrew/versions
-    brew unlink gcc
-    brew install gcc48 
-    brew install boost 
-    brew install cmake 
+    #brew unlink gcc
+    #brew install gcc49 
+    #brew install boost 
+    #brew install cmake 
 
     export CC=gcc-4.8
     export CXX=g++-4.8
