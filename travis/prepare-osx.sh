d1a6d15cb80620ab6d8ecc83a779a02ab7706302
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-03-17 22:00:24 -0400

    remove virtual env?

--- travis/prepare-osx.sh
+++ travis/prepare-osx.sh
@@ -8,25 +8,25 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     hash -r
 
     #pip install --upgrade virtualenv
-    pip install --upgrade -e git+https://github.com/pypa/virtualenv.git#egg=virtualenv
+    #pip install --upgrade -e git+https://github.com/pypa/virtualenv.git#egg=virtualenv
 
-    case "${TRAVIS_OSX_PYTHON_VERSION}" in
-        py27)
-            # Install some custom Python 2.7 requirements on OS X
-            #brew install python
-            if [ ! -e $HOME/virtualenv/bin/activate ]; then
-                cd $HOME
-                virtualenv venv --no-setuptools --no-pip --no-wheel
-            fi
-            ;;
-        py35)
-            # Install some custom Python 3.5 requirements on OS X
-            #brew install python3
-            if [ ! -e $HOME/virtualenv/bin/activate ]; then
-                cd $HOME
-                #pyvenv virtualenv
-                virtualenv venv -p $(which python3) --no-setuptools --no-pip --no-wheel
-            fi
-            ;;
-    esac
+    # case "${TRAVIS_OSX_PYTHON_VERSION}" in
+    #     py27)
+    #         # Install some custom Python 2.7 requirements on OS X
+    #         #brew install python
+    #         if [ ! -e $HOME/virtualenv/bin/activate ]; then
+    #             cd $HOME
+    #             #virtualenv venv --no-setuptools --no-pip --no-wheel
+    #         fi
+    #         ;;
+    #     py35)
+    #         # Install some custom Python 3.5 requirements on OS X
+    #         #brew install python3
+    #         if [ ! -e $HOME/virtualenv/bin/activate ]; then
+    #             cd $HOME
+    #             #pyvenv virtualenv
+    #             #virtualenv venv -p $(which python3) --no-setuptools --no-pip --no-wheel
+    #         fi
+    #         ;;
+    # esac
 fi
