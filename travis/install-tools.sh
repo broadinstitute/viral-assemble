0b2c5be5e65e2154595b9ec78fc2debcbdcf8cf0
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-03-18 12:41:01 -0400

    unlink boost first

--- travis/install-tools.sh
+++ travis/install-tools.sh
@@ -16,6 +16,7 @@ if [ ! -d $GATK_PATH -o ! -d $NOVOALIGN_PATH ]; then
   fi
 fi
 
+# perhaps not needed?
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
   export CC=gcc-4.9
   export CXX=g++-4.9
--- travis/prepare-osx.sh
+++ travis/prepare-osx.sh
@@ -5,6 +5,7 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew update > /dev/null
     brew tap homebrew/versions
     brew unlink gcc
+    brew unlink boost
     brew install gcc49 
     brew install boost 
     brew install cmake 
