27caad43697f6c7ed6aeef02db4f3f0695558590
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2016-05-08 19:25:09 -0400

    asmcmp: plotting histograms of diffs

--- eval/asmcmp.py
+++ eval/asmcmp.py
@@ -1,6 +1,6 @@
 """Code for comparing assemblies"""
 
-import os
+import os, pandas, logging
 import asmutil
 
 class AsmCmp(object):
@@ -20,7 +20,8 @@ class AsmCmp(object):
 
     def __get_stat_fname( self, sample, asm, stat ):
         """Return the name of the file where the given statistic is stored"""
-        return os.path.join( self.cfg['statdir'], sample, asm, stat + '.dat' )
+        return os.path.join( self.cfg['statdir'], asmutil.make_alpha_num( sample ), 
+                             asmutil.make_alpha_num( asm ), asmutil.make_alpha_num( stat ) + '.dat' )
 
     def find_largest_changes( self, asm1, asm2, samples, stats, out_dir ):
         """For a pair of assemblers, find the largest differences between their performance."""
@@ -31,22 +32,43 @@ class AsmCmp(object):
                 stat_asm1 = self.get_stat( sample, asm1, stat )
                 stat_asm2 = self.get_stat( sample, asm2, stat )
                 if stat_asm1 is not None and stat_asm2 is not None:
-                    deltas.append( stat_asm1 - stat_asm2 )
+                    diff = stat_asm1 - stat_asm2
+                    if diff != 0:
+                        logging.info( 'sample=%s stat=%s diff=%f', sample, stat, diff )
+                    deltas.append( diff  )
 
             import matplotlib as mp
             mp.use('agg')
             import matplotlib.pyplot as pp
 
-            pp.figure()
+            fig=pp.figure()
             pp.xlabel( 'delta ' + stat )
             pp.title( asm1 + ' vs. ' + asm2 )
             pp.hist(deltas)
             asmutil.ensure_dir_exists( out_dir )
-            pp.savefig( os.path.join( out_dir, stat + '.png' ) )
+            pp.savefig( os.path.join( out_dir, asmutil.make_alpha_num(stat) + '.png' ) )
+            pp.close(fig)
+
+    def import_quast_stats( self, quast_dir, asms ):
+        """Import assembly stats computed by QUAST"""
+        
+        logging.info('quast_dir='+quast_dir)
+        for sample in os.listdir(quast_dir):
+            logging.info('sample='+sample)
+            z = pandas.read_csv( os.path.join( quast_dir, sample, 'report.tsv' ), sep = '\t' )
+            for t in z.itertuples(index=False):
+                for asm_idx, asm in enumerate(asms):
+                    value=t[1+asm_idx]
+                    stat=t[0]
+                    if asmutil.is_convertible_to(value,float):
+                        # logging.info('sample=%s stat=%s asm_idx=%d asm=%s value=%s',
+                        #              sample, stat, asm_idx, asm, value)
+                        self.put_stat(sample=sample, asm=asm, stat=stat, value=value)
 
 # end: class AsmCmp
 
 def test_AsmCmp():
+    logging.basicConfig(level=logging.INFO)
     statdir = '/broad/hptmp/ilya/asmstats'
     asmutil.ensure_dir_exists( statdir )
     cfg = dict( statdir = statdir )
@@ -59,6 +81,16 @@ def test_AsmCmp():
     asmcmp.find_largest_changes( asm1 = 'NOVALIGN_1', asm2 = 'NOVALIGN_2', 
                                  samples = ( 'READS1', 'READS2' ),
                                  stats = [ 'NA50' ], out_dir = os.path.join( statdir, 'plots' ) )
+    quast_dir='/idi/sabeti-scratch/ilya/sw/quast-3.2/ebov/assembly-vfat__assembly-mummer3/quast/assembly-vfat__assembly-mummer3'
+    samples=os.listdir(quast_dir)
+    stats=list(pandas.read_csv(os.path.join(quast_dir,samples[0],'report.tsv'),sep='\t',header=0).ix[:,0])
+    print('stats=',stats)
+    asmcmp.import_quast_stats( quast_dir=quast_dir, asms='vfat mummer3'.split() )
+    logging.info('-------------finding largest changes----------------')
+    asmcmp.find_largest_changes( asm1='vfat', asm2='mummer3', samples=samples, stats=stats,
+                                 out_dir=os.path.join(statdir,'plots2'))
+
+    
 
 if __name__ == '__main__':
     test_AsmCmp()
--- eval/asmutil.py
+++ eval/asmutil.py
@@ -1,6 +1,6 @@
 """Misc assembly-related utils"""
 
-import os
+import os, re
 
 def dump_file( fname, value ):
     """store string in file"""
@@ -18,3 +18,15 @@ def ensure_dir_exists( d ):
     """Create the given dir (and any parent dirs) if it does not already exist"""
     if not os.path.isdir( d ): os.makedirs( d )
 
+def make_alpha_num( s ):
+    """Return a version of the argument string, in which all non-alphanumeric chars have been replaced
+    by underscores.
+    """
+    return re.sub( '\W+', '_', s )
+    
+def is_convertible_to( x, aType ):
+    """Test if a value is convertible to a given type"""
+    try:
+        dummy = aType(x)
+        return True
+    except ValueError: return False
