f61204902d55892bf0dd3e745b2268d9060846b6
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2017-05-20 21:19:59 -0400

    removed the eval/ directory

--- eval/run_quast.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e -o pipefail
-source activate py27
-/idi/sabeti-scratch/ilya/sw/quast-3.2/quast.py "$@"
-
-
--- eval/test_quast.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e -o pipefail
-pushd /idi/sabeti-scratch/ilya/sw/quast-3.2
-/idi/sabeti-scratch/ilya/viral-ngs/eval/run_quast.sh --test
-
-
