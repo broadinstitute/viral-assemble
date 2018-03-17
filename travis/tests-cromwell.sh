c49378499ece027511db44737ca07d07763377ee
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2018-03-17 02:21:32 -0400

    more cromwell test fixes

--- travis/tests-cromwell.sh
+++ travis/tests-cromwell.sh
@@ -3,19 +3,21 @@ set -x -e  # intentionally allow for pipe failures below
 
 ln -s $GATK_PATH/GenomeAnalysisTK.jar .
 mkdir -p workflows
-ln -s pipes/WDL/workflows/*.wdl pipes/WDL/workflows/tasks/*.wdl workflows
+cp cromwell.jar pipes/WDL/workflows/*.wdl pipes/WDL/workflows/tasks/*.wdl workflows
 cd workflows
 
 for workflow in ../pipes/WDL/workflows/*.wdl; do
 	workflow_name=$(basename $workflow .wdl)
-	input_json="../test/input/WDL/test_inputs-$workflow_name-local.json"
+	input_json_basename="test_inputs-$workflow_name-local.json"
+	input_json="../test/input/WDL/$input_json_basename"
 	if [ -f $input_json ]; then
 		date
 		echo "Executing $workflow_name using Cromwell on local instance"
+		cp $input_json .
 		# the "cat" is to allow a pipe failure (otherwise it halts because of set -e)
-		java -jar ../cromwell.jar run \
+		java -jar cromwell.jar run \
 			$workflow_name.wdl \
-			-i $input_json | tee cromwell.out
+			-i $input_json_basename | tee cromwell.out
 		if [ ${PIPESTATUS[0]} -gt 0 ]; then
 			echo "error running $workflow_name"
 			error_logs=$(grep stderr cromwell.out | perl -lape 's/.*\s(\S+)$/$1/g')
