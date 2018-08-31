db1fdaf91ac7f056e47a554d66a599d53dd81763
Ilya Shlyakhter <ilya@broadinstitute.org>
2018-08-31 11:38:47 -0400

    renamed docker_tag to docker_img; use originalInputs if runInputs do not give a value for a non-optional input

--- workflow_utils.py
+++ workflow_utils.py
@@ -38,10 +38,10 @@ def _pretty_print_json(json_dict):
 # given a dx analysis, run it locally
 #
 
-def get_workflow_inputs(workflow_name, t_dir, docker_tag):
+def get_workflow_inputs(workflow_name, t_dir, docker_img):
     """Run womtool to get the inputs of the wdl workflow"""
     with util.file.pushd_popd(t_dir):
-        subprocess.check_call('docker run --rm ' + docker_tag + ' tar cf - source/pipes/WDL > wdl.tar', shell=True)
+        subprocess.check_call('docker run --rm ' + docker_img + ' tar cf - source/pipes/WDL > wdl.tar', shell=True)
         subprocess.check_call('tar xvf wdl.tar', shell=True)
         for f in glob.glob('source/pipes/WDL/workflows/*.wdl'):
             shutil.copy(f, '.')
@@ -53,7 +53,7 @@ def get_workflow_inputs(workflow_name, t_dir, docker_tag):
 def _get_dx_val(val, dx_cache = '/dev/shm/dxcache'):
     """Resolve a dx value: if it is a scalar, just return that;
     if it is a dx file, fetch the file, cache it locally, and return the path to the file."""
-    
+
     print('parsing val: ', val)
     util.file.mkdir_p(dx_cache)
     if isinstance(val, collections.Mapping) and '$dnanexus_link' in val:
@@ -82,14 +82,14 @@ def _get_dx_val(val, dx_cache = '/dev/shm/dxcache'):
     else:
         return val
 
-def _parse_cromwell_output(cromwell_output_str):
+def _parse_cromwell_output_str(cromwell_output_str):
     """Parse cromwell output"""
     assert cromwell_output_str.count('Final Outputs:') == 1
     json_beg = cromwell_output_str.index('Final Outputs:') + len('Final Outputs:')
     json_end = cromwell_output_str.index('\n}\n', json_beg) + 2
     return json.loads(cromwell_output_str[json_beg:json_end])
 
-def run_dx_locally(workflow_name, analysis_dxid, docker_tag, output_dir):
+def run_dx_locally(workflow_name, analysis_dxid, docker_img, output_dir):
     """Run a dx analysis locally, using a specified version of viral-ngs.
 
     Notes:
@@ -97,16 +97,17 @@ def run_dx_locally(workflow_name, analysis_dxid, docker_tag, output_dir):
        - record the actual git hash (since git tag might change)
 
     """
-    subprocess.check_call('docker pull ' + docker_tag, shell=True)
+    subprocess.check_call('docker pull ' + docker_img, shell=True)
 
     analysis = dxpy.DXAnalysis(dxid=analysis_dxid)
     analysis_descr = analysis.describe()
 
     with util.file.tmp_dir('_workflow_copy') as t_dir:
     
-        wdl_wf_inputs = get_workflow_inputs(workflow_name, t_dir, docker_tag=docker_tag)
+        wdl_wf_inputs = get_workflow_inputs(workflow_name, t_dir, docker_img=docker_img)
 
         dx_wf_inputs = { k.split('.')[-1] : v for k, v in analysis_descr['runInput'].items()}   # check for dups
+        dx_wf_orig_inputs = { k.split('.')[-1] : v for k, v in analysis_descr['originalInput'].items()}   # check for dups
         print('\n'.join(dx_wf_inputs.keys()))
         new_wdl_wf_inputs = {}
         for wdl_wf_input, wdl_wf_input_descr in wdl_wf_inputs.items():
@@ -114,13 +115,18 @@ def run_dx_locally(workflow_name, analysis_dxid, docker_tag, output_dir):
             wdl_wf_input = wdl_wf_input.split('.')[-1]
             if wdl_wf_input in dx_wf_inputs:
                 print('HAVE', wdl_wf_input, wdl_wf_input_descr, dx_wf_inputs[wdl_wf_input])
-                # TODO: only use that input if 
                 dx_wf_input = dx_wf_inputs[wdl_wf_input]
                 new_wdl_wf_inputs[wdl_wf_input_full] = map(_get_dx_val, dx_wf_input) \
                                                        if isinstance(dx_wf_input, list) \
                                                           else _get_dx_val(dx_wf_input)
+            elif '(optional' not in wdl_wf_input_descr and wdl_wf_input in dx_wf_orig_inputs:
+                print('HAVE', wdl_wf_input, wdl_wf_input_descr, dx_wf_orig_inputs[wdl_wf_input])
+                dx_wf_input = dx_wf_orig_inputs[wdl_wf_input]
+                new_wdl_wf_inputs[wdl_wf_input_full] = map(_get_dx_val, dx_wf_input) \
+                                                       if isinstance(dx_wf_input, list) \
+                                                          else _get_dx_val(dx_wf_input)
             else:
-                #print('MISSING', wdl_wf_input, wdl_wf_input_descr)
+                print('MISSING', wdl_wf_input, wdl_wf_input_descr)
                 assert '(optional' in wdl_wf_input_descr
 
 
@@ -132,11 +138,11 @@ def run_dx_locally(workflow_name, analysis_dxid, docker_tag, output_dir):
             with open('wf.json', 'wt') as wf_out:
                 json.dump(new_wdl_wf_inputs, wf_out, indent=4, separators=(',', ': '))
 
-            if docker_tag:
+            if docker_img:
                 # TODO: option to update just some of the tasks.
                 # actually, when compiling WDL, should have this option -- or, actually,
                 # should make a new workflow where older apps are reused for stages that have not changed.
-                subprocess.check_call('sed -i -- "s|{}|{}|g" *.wdl'.format('quay.io/broadinstitute/viral-ngs', docker_tag),
+                subprocess.check_call('sed -i -- "s|{}|{}|g" *.wdl'.format('quay.io/broadinstitute/viral-ngs', docker_img),
                                       shell=True)
 
             shutil.rmtree(output_dir, ignore_errors=True)
@@ -154,6 +160,8 @@ def run_dx_locally(workflow_name, analysis_dxid, docker_tag, output_dir):
             with open('wf_opts.json', 'wt') as wf_opts_file:
                 json.dump(wf_opts_dict, wf_opts_file, indent=4, separators=(',', ': '))
 
+            # add cromwell labels: dx project, the docker tag we ran on, etc.
+
             _log.info('Validating workflow')
             subprocess.check_call('womtool validate -i wf.json ' + workflow_name + '.wdl', shell=True)
             _log.info('Validated workflow; calling cromwell')
@@ -163,7 +171,7 @@ def run_dx_locally(workflow_name, analysis_dxid, docker_tag, output_dir):
                                                      shell=True)
             _log.info('Cromwell returned')
 
-            cromwell_output = _parse_cromwell_output(wdl_result_str)
+            cromwell_output = _parse_cromwell_output_str(wdl_result_str)
             #print('WDL_RESULT_STR=', wdl_result_str)
             util.file.dump_file(os.path.join(output_dir, 'wdl_output.txt'), wdl_result_str)
             
@@ -175,6 +183,16 @@ def run_dx_locally(workflow_name, analysis_dxid, docker_tag, output_dir):
             # print(json.dumps(wdl_result, indent=4, separators=(',', ': ')))
             # print('---------------------------')
 
+def record_run_results(docker_img, cromwell_output):
+    #asdf
+
+    # so, we also need to generate a unique id for this run;
+    # since there can be multiple runs.
+
+    # also, need to see how to handle failures, full or partial.
+    #
+
+    pass
 
 # =======================
 def full_parser():
@@ -182,9 +200,9 @@ def full_parser():
 
 
 if __name__ == '__main__':
-    #print(_pretty_print_json(_parse_cromwell_output(util.file.slurp_file('/dev/shm/cromwell_out/wdl_output.txt'))))
+    #print(_pretty_print_json(_parse_cromwell_output_str(util.file.slurp_file('/dev/shm/cromwell_out/wdl_output.txt'))))
     if True:
         run_dx_locally(workflow_name='assemble_denovo', analysis_dxid='analysis-FJfqjg005Z3Vp5Q68jxzx5q1',
-                       docker_tag='quay.io/broadinstitute/viral-ngs',
+                       docker_img='quay.io/broadinstitute/viral-ngs',
                        output_dir='/dev/shm/cromwell_out')
     #util.cmd.main_argparse(__commands__, __doc__)
