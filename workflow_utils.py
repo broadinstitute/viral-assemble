99a3dde4357b8abf352433acdaff67c4171d7bc3
Ilya Shlyakhter <ilya@broadinstitute.org>
2018-09-04 21:04:28 -0400

    WIP - Added git-annex-based saving of results.  Various things are hard-coded, need to be made configurable.

--- workflow_utils.py
+++ workflow_utils.py
@@ -35,11 +35,16 @@ logging.basicConfig(format="%(asctime)s - %(module)s:%(lineno)d:%(funcName)s - %
 _log.setLevel(logging.DEBUG)
 
 def workflow_utils_init():
-    """Install the dependencies: cromwell and dxpy."""
-    subprocess.check_call('conda install cromwell dxpy')
+    """Install the dependencies: cromwell and dxpy and git-annex."""
+    subprocess.check_call('conda install cromwell dxpy git-annex')
 
 def _pretty_print_json(json_dict):
     return json.dumps(json_dict, indent=4, separators=(',', ': '))
+
+def _run(cmd):
+    print('running command: ', cmd)
+    subprocess.check_call(cmd, shell=True)
+    print('command succeeded: ', cmd)
 
 #
 # given a dx analysis, run it locally
@@ -48,8 +53,8 @@ def _pretty_print_json(json_dict):
 def get_workflow_inputs(workflow_name, t_dir, docker_img):
     """Run womtool to get the inputs of the wdl workflow"""
     with util.file.pushd_popd(t_dir):
-        subprocess.check_call('docker run --rm ' + docker_img + ' tar cf - source/pipes/WDL > wdl.tar', shell=True)
-        subprocess.check_call('tar xvf wdl.tar', shell=True)
+        _run('docker run --rm ' + docker_img + ' tar cf - source/pipes/WDL > wdl.tar')
+        _run('tar xvf wdl.tar')
         for f in glob.glob('source/pipes/WDL/workflows/*.wdl'):
             shutil.copy(f, '.')
         for f in glob.glob('source/pipes/WDL/workflows/tasks/*.wdl'):
@@ -96,7 +101,7 @@ def _parse_cromwell_output_str(cromwell_output_str):
     json_end = cromwell_output_str.index('\n}\n', json_beg) + 2
     return json.loads(cromwell_output_str[json_beg:json_end])
 
-def run_dx_locally(workflow_name, analysis_dxid, docker_img, output_dir):
+def run_dx_locally(workflow_name, analysis_dxid, docker_img):
     """Run a dx analysis locally, using a specified version of viral-ngs.
 
     Notes:
@@ -104,98 +109,116 @@ def run_dx_locally(workflow_name, analysis_dxid, docker_img, output_dir):
        - record the actual git hash (since git tag might change)
 
     """
-    subprocess.check_call('docker pull ' + docker_img, shell=True)
+    analysis_id = create_run_id()
+
+    _run('docker pull ' + docker_img)
 
     analysis = dxpy.DXAnalysis(dxid=analysis_dxid)
     analysis_descr = analysis.describe()
 
-    with util.file.tmp_dir('_workflow_copy') as t_dir:
-    
-        wdl_wf_inputs = get_workflow_inputs(workflow_name, t_dir, docker_img=docker_img)
+    with util.file.tmp_dir('_workflow_copy') as t_dir_orig:
+        with util.file.pushd_popd(t_dir_orig):
+            _run('git clone /ndata/cromwell-workflow-logs ' + analysis_id)
+            t_dir_git = os.path.join(t_dir_orig, analysis_id)
+            util.file.mkdir_p(t_dir_git)
+            with util.file.pushd_popd(t_dir_git):
+                _run('git annex init')
+                t_dir = os.path.join(t_dir_git, analysis_id)
+                util.file.mkdir_p(t_dir)
+                with util.file.pushd_popd(t_dir):
+                    print('TTTTTTTTTTT t_dir=', t_dir)
+                    output_dir = os.path.join(t_dir, 'output')
 
-        dx_wf_inputs = { k.split('.')[-1] : v for k, v in analysis_descr['runInput'].items()}   # check for dups
-        dx_wf_orig_inputs = { k.split('.')[-1] : v for k, v in analysis_descr['originalInput'].items()}   # check for dups
-        print('\n'.join(dx_wf_inputs.keys()))
-        new_wdl_wf_inputs = {}
-        for wdl_wf_input, wdl_wf_input_descr in wdl_wf_inputs.items():
-            wdl_wf_input_full = wdl_wf_input
-            wdl_wf_input = wdl_wf_input.split('.')[-1]
-            if wdl_wf_input in dx_wf_inputs:
-                print('HAVE', wdl_wf_input, wdl_wf_input_descr, dx_wf_inputs[wdl_wf_input])
-                dx_wf_input = dx_wf_inputs[wdl_wf_input]
-                new_wdl_wf_inputs[wdl_wf_input_full] = map(_get_dx_val, dx_wf_input) \
-                                                       if isinstance(dx_wf_input, list) \
-                                                          else _get_dx_val(dx_wf_input)
-            elif '(optional' not in wdl_wf_input_descr and wdl_wf_input in dx_wf_orig_inputs:
-                print('HAVE', wdl_wf_input, wdl_wf_input_descr, dx_wf_orig_inputs[wdl_wf_input])
-                dx_wf_input = dx_wf_orig_inputs[wdl_wf_input]
-                new_wdl_wf_inputs[wdl_wf_input_full] = map(_get_dx_val, dx_wf_input) \
-                                                       if isinstance(dx_wf_input, list) \
-                                                          else _get_dx_val(dx_wf_input)
-            else:
-                print('MISSING', wdl_wf_input, wdl_wf_input_descr)
-                assert '(optional' in wdl_wf_input_descr
+                    wdl_wf_inputs = get_workflow_inputs(workflow_name, t_dir, docker_img=docker_img)
+
+                    dx_wf_inputs = { k.split('.')[-1] : v for k, v in analysis_descr['runInput'].items()}   # check for dups
+                    dx_wf_orig_inputs = { k.split('.')[-1] : v for k, v in analysis_descr['originalInput'].items()}   # check for dups
+                    print('\n'.join(dx_wf_inputs.keys()))
+                    new_wdl_wf_inputs = {}
+                    for wdl_wf_input, wdl_wf_input_descr in wdl_wf_inputs.items():
+                        wdl_wf_input_full = wdl_wf_input
+                        wdl_wf_input = wdl_wf_input.split('.')[-1]
+                        if wdl_wf_input in dx_wf_inputs:
+                            print('HAVE', wdl_wf_input, wdl_wf_input_descr, dx_wf_inputs[wdl_wf_input])
+                            dx_wf_input = dx_wf_inputs[wdl_wf_input]
+                            new_wdl_wf_inputs[wdl_wf_input_full] = map(_get_dx_val, dx_wf_input) \
+                                                                   if isinstance(dx_wf_input, list) \
+                                                                      else _get_dx_val(dx_wf_input)
+                        elif '(optional' not in wdl_wf_input_descr and wdl_wf_input in dx_wf_orig_inputs:
+                            print('HAVE', wdl_wf_input, wdl_wf_input_descr, dx_wf_orig_inputs[wdl_wf_input])
+                            dx_wf_input = dx_wf_orig_inputs[wdl_wf_input]
+                            new_wdl_wf_inputs[wdl_wf_input_full] = map(_get_dx_val, dx_wf_input) \
+                                                                   if isinstance(dx_wf_input, list) \
+                                                                      else _get_dx_val(dx_wf_input)
+                        else:
+                            print('MISSING', wdl_wf_input, wdl_wf_input_descr)
+                            assert '(optional' in wdl_wf_input_descr
 
 
-        print(_pretty_print_json(new_wdl_wf_inputs))
+                    print(_pretty_print_json(new_wdl_wf_inputs))
 
-        ################# put in the right docker ID!!  and find a place to keep the docker cache.
+                    ################# put in the right docker ID!!  and find a place to keep the docker cache.
 
-        with util.file.pushd_popd(t_dir):
-            with open('wf.json', 'wt') as wf_out:
-                json.dump(new_wdl_wf_inputs, wf_out, indent=4, separators=(',', ': '))
+                    with open('wf.json', 'wt') as wf_out:
+                        json.dump(new_wdl_wf_inputs, wf_out, indent=4, separators=(',', ': '))
 
-            if docker_img:
-                # TODO: option to update just some of the tasks.
-                # actually, when compiling WDL, should have this option -- or, actually,
-                # should make a new workflow where older apps are reused for stages that have not changed.
-                subprocess.check_call('sed -i -- "s|{}|{}|g" *.wdl'.format('quay.io/broadinstitute/viral-ngs', docker_img),
-                                      shell=True)
+                    if docker_img:
+                        # TODO: option to update just some of the tasks.
+                        # actually, when compiling WDL, should have this option -- or, actually,
+                        # should make a new workflow where older apps are reused for stages that have not changed.
+                        _run('sed -i -- "s|{}|{}|g" *.wdl'.format('quay.io/broadinstitute/viral-ngs', docker_img))
 
-            shutil.rmtree(output_dir, ignore_errors=True)
-            assert not os.path.exists(output_dir)
-            util.file.mkdir_p(output_dir)
-            util.file.mkdir_p(os.path.join(output_dir, 'outputs'))
-            util.file.mkdir_p(os.path.join(output_dir, 'logs'))
-            util.file.mkdir_p(os.path.join(output_dir, 'call_logs'))
-            #util.file.mkdir_p(os.path.join(output_dir, 'metadata'))
-            wf_opts_dict = { "final_workflow_outputs_dir": os.path.join(output_dir, 'outputs'),
-                             "final_workflow_log_dir": os.path.join(output_dir, 'logs'),
-                             "final_call_logs_dir": os.path.join(output_dir, 'call_logs')
-            }
-                             
-            with open('wf_opts.json', 'wt') as wf_opts_file:
-                json.dump(wf_opts_dict, wf_opts_file, indent=4, separators=(',', ': '))
+                    shutil.rmtree(output_dir, ignore_errors=True)
+                    assert not os.path.exists(output_dir)
+                    util.file.mkdir_p(output_dir)
+                    util.file.mkdir_p(os.path.join(output_dir, 'outputs'))
+                    util.file.mkdir_p(os.path.join(output_dir, 'logs'))
+                    util.file.mkdir_p(os.path.join(output_dir, 'call_logs'))
+                    #util.file.mkdir_p(os.path.join(output_dir, 'metadata'))
+                    wf_opts_dict = { "final_workflow_outputs_dir": os.path.join(output_dir, 'outputs'),
+                                     "final_workflow_log_dir": os.path.join(output_dir, 'logs'),
+                                     "final_call_logs_dir": os.path.join(output_dir, 'call_logs')
+                    }
 
-            # add cromwell labels: dx project, the docker tag we ran on, etc.
+                    with open('wf_opts.json', 'wt') as wf_opts_file:
+                        json.dump(wf_opts_dict, wf_opts_file, indent=4, separators=(',', ': '))
 
-            _log.info('Validating workflow')
-            subprocess.check_call('womtool validate -i wf.json ' + workflow_name + '.wdl', shell=True)
-            _log.info('Validated workflow; calling cromwell')
-            wdl_result_str = subprocess.check_output('cromwell run ' + workflow_name + \
-                                                     '.wdl -i wf.json -o wf_opts.json' + \
-                                                     ' -m ' + os.path.join(output_dir, 'metadata'),
-                                                     shell=True)
-            _log.info('Cromwell returned')
+                    # add cromwell labels: dx project, the docker tag we ran on, etc.
 
-            cromwell_output = _parse_cromwell_output_str(wdl_result_str)
-            #print('WDL_RESULT_STR=', wdl_result_str)
-            util.file.dump_file(os.path.join(output_dir, 'wdl_output.txt'), wdl_result_str)
-            
-                                      
-            # wdl_result = json.loads(wdl_result_str)
-            # print('---------------------------')
-            # print('WDL RESULT:')
-            # print('---------------------------')
-            # print(json.dumps(wdl_result, indent=4, separators=(',', ': ')))
-            # print('---------------------------')
+                    _log.info('Validating workflow')
+                    _run('womtool validate -i wf.json ' + workflow_name + '.wdl')
+                    _log.info('Validated workflow; calling cromwell')
+                    wdl_result_str = subprocess.check_output('cromwell run ' + workflow_name + \
+                                                             '.wdl -i wf.json -o wf_opts.json' + \
+                                                             ' -m ' + os.path.join(output_dir, 'metadata'),
+                                                             shell=True)
+                    _log.info('Cromwell returned')
 
+                    cromwell_output = _parse_cromwell_output_str(wdl_result_str)
+                    #print('WDL_RESULT_STR=', wdl_result_str)
+                    util.file.dump_file(os.path.join(output_dir, 'wdl_output.txt'), wdl_result_str)
+
+
+                    # wdl_result = json.loads(wdl_result_str)
+                    # print('---------------------------')
+                    # print('WDL RESULT:')
+                    # print('---------------------------')
+                    # print(json.dumps(wdl_result, indent=4, separators=(',', ': ')))
+                    # print('---------------------------')
+
+                _run('sudo chown -R ec2-user * || true')
+                _run('chmod -R u+w *')
+                _run('git annex add')
+                _run('git commit -m after_running_analysis_' + analysis_id + '.')
+                _run('git annex initremote content type=directory directory=/ndata/git-annex-content/ encryption=none')
+                _run('git annex copy --all --to content')
+                _run('git annex dead here')
+                _run('git annex sync')
 
 def create_run_id():
     """Generate a unique ID for a run (set of steps run as part of one workflow)."""
     return util.file.string_to_file_name('-'.join(map(str, 
-                                                       ('analysis-', time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())[2:], 
-                                                        getpass.getuser(),
+                                                       ('analysis', time.strftime('%Y%m%d-%H%M%S', time.localtime())[2:], 
                                                         uuid.uuid4()))))[:1024]
 
 def get_docker_hash(docker_img):
@@ -210,14 +233,20 @@ def get_docker_hash(docker_img):
     assert digest.startswith('sha256:') and len(digest) == 71
     return digest
 
-# def record_run_results(docker_img, cromwell_output):
-#     #asdf
+def hash_and_store_file(fname):
+    '''Store the given file in a key-value store, '''
+    pass
 
-#     # so, we also need to generate a unique id for this run;
-#     # since there can be multiple runs.
+def gather_run_results(docker_img, cromwell_output):
+    pass
+    
+    #asdf
 
-#     # also, need to see how to handle failures, full or partial.
-#     #
+    # so, we also need to generate a unique id for this run;
+    # since there can be multiple runs.
+
+    # also, need to see how to handle failures, full or partial.
+    #
 #     sdb_client = boto3.client('sdb')
 #    response = sdb_client.put_attributes(DomainName='viral_ngs_benchmarks',
 #                                         ItemName=create_run_id(),
@@ -236,6 +265,5 @@ if __name__ == '__main__':
     #record_run_results(0,0)
     if True:
         run_dx_locally(workflow_name='assemble_denovo', analysis_dxid='analysis-FJfqjg005Z3Vp5Q68jxzx5q1',
-                       docker_img='quay.io/broadinstitute/viral-ngs',
-                       output_dir=os.path.join('/data/benchruns', create_run_id()))
+                       docker_img='quay.io/broadinstitute/viral-ngs')
     #util.cmd.main_argparse(__commands__, __doc__)
