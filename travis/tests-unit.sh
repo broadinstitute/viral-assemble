bc5969467c1c76bcc22c527faa6c6b8b1bbeaa47
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2018-12-11 00:42:41 -0500

    re-enabling tests

--- travis/tests-unit.sh
+++ travis/tests-unit.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#pytest --cov-append test/unit
-pytest --log-cli-level=DEBUG -vvvsx test/unit/test_workflow_utils.py::test_git_annex_basic
+pytest --cov-append test/unit
 
 rc=$?; if [[ $rc != 0 ]]; then sleep 10; exit $rc; fi
 # sleep to allow logs to be printed without truncation in the event of error
