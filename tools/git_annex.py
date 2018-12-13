56796ccb30eac468ae3515c25e3c4765d269e462
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2018-12-13 16:17:18 -0500

    tools/git-annex - added comments

--- tools/git_annex.py
+++ tools/git_annex.py
@@ -2,6 +2,8 @@
     Tool wrapper for git-annex
 '''
 
+# * imports
+
 import logging
 import os
 import os.path
@@ -23,34 +25,22 @@ TOOL_VERSION = '7.20181105'
 _log = logging.getLogger(__name__)
 _log.setLevel(logging.DEBUG)
 
-
-def _noquote(s):
-    return '_noquote:' + str(s)
-
-def _quote(s):
-    s = str(s)
-    if s.startswith('_noquote:'): return s[len('_noquote:'):]
-    return pipes.quote(s) if hasattr(pipes, 'quote') else shlex.quote(s)
-
-def _make_cmd(cmd, *args):
-    _log.debug('ARGS=%s', args)
-    return ' '.join([cmd] + [_quote(str(arg)) for arg in args if arg not in (None, '')])
-
-def _run(cmd, *args):
-    cmd = _make_cmd(cmd, *args)
-    _log.info('running command: %s cwd=%s', cmd, os.getcwd())
-    beg_time = time.time()
-    subprocess.check_call(cmd, shell=True)
-    _log.info('command succeeded in {}s: {}'.format(time.time()-beg_time, cmd))
-
+# * class GitAnnexTool
 class GitAnnexTool(tools.Tool):
 
-    '''Tool wrapper for git-annex'''
+    '''Tool wrapper for git-annex.
+
+    TODO:
+       - support unlocked annexed files
+    '''
 
     def __init__(self, install_methods=None):
         if install_methods is None:
             install_methods = [tools.CondaPackage(TOOL_NAME, version=TOOL_VERSION, channel='conda-forge',
                                                   executable='git-annex',
+                                                  # install git-annex into own environment, because it has
+                                                  # some dependency requirements (like specific Perl version)
+                                                  # that conflict with other pkgs.
                                                   env='vngs-git-annex-env')]
         tools.Tool.__init__(self, install_methods=install_methods)
 
@@ -58,8 +48,11 @@ class GitAnnexTool(tools.Tool):
         return TOOL_VERSION
 
     def execute(self, args, tool='git-annex'):    # pylint: disable=W0221
+        """Run program `tool` with arguments `args`."""
         bin_dir = os.path.dirname(self.install_and_get_path())
         tool_cmd = [os.path.join(bin_dir, tool)] + list(map(str, args))
+
+        # ensure that the git and git-annex binaries we installed are first in PATH.
         env = dict(os.environ)
         env['PATH'] = bin_dir + ':' + env['PATH']
         _log.debug(' '.join(tool_cmd))
@@ -67,27 +60,34 @@ class GitAnnexTool(tools.Tool):
         subprocess.check_call(tool_cmd, env=env)
 
     def execute_git(self, args):
+        """Run a git command"""
         self.execute(args, tool='git')
 
     def print_version(self):
+        """Print git-annex version"""
         self.execute(['version'])
 
     def init_repo(self):
+        """Initialize git-annex repo in current dir"""
         self.execute_git(['init'])
         self.execute(['init'])
 
     def add(self, fname):
+        """Add a file to git-annex"""
         self.execute(['add', fname])
 
     def commit(self, msg):
+        """Execute git commit"""
         self.execute_git(['commit', '-m', '"{}"'.format(msg)])
 
     def initremote(self, name, remote_type, **kw):
+        """Initialize a git-annex special remote"""
         remote_attrs = dict(kw)
         if 'encryption' not in remote_attrs: remote_attrs['encryption'] = 'none'
         self.execute(['initremote', name, 'type='+remote_type]+['='.join((k,v)) for k, v in remote_attrs.items()])
 
     def move(self, fname, to_remote_name):
+        """Move a file from local repo to a remote"""
         self.execute(['move', fname, '--to', to_remote_name])
 
     def _get_link_into_annex(self, f):
@@ -99,6 +99,8 @@ class GitAnnexTool(tools.Tool):
         (link_into_annex, target_of_link_into_annex), where link_into_annex is the path to the
         symlink that points directly into the annex (i.e. the penultimate link in a chain of symlinks),
         while target_of_link_into_annex is the path within the annex to which link_into_annex points.
+
+        TODO: handle unlocked git-annex files.
         """
         annex_link_target = None
         f_cur = f
@@ -123,17 +125,18 @@ class GitAnnexTool(tools.Tool):
         return f, annex_link_target
 
     def is_annexed_file(self, f):
+        """Test is `f` is a file controlled by git-annex."""
         return os.path.lexists(f) and not os.path.isdir(f) and self._get_link_into_annex(f)[1]
 
     def get_annexed_file_attrs(self, f):
         raise NotImplemented()
 
     def get(self, f):
-        """Ensure the file exists in the local annex.  Unlike git-annex-get, follows symlinks and 
-        will get the file regardless of what the current dir is."""
+        """Ensure the file exists in the local annex, fetching it from a remote if necessary.
+        Unlike git-annex-get, follows symlinks and  will get the file regardless of what the current dir is."""
 
         # TODO: what if f is a dir, or list of dirs?  including symlinks?
-        # then, to preserve git-annex-get semantics, need to 
+        # then, to preserve git-annex-get semantics, need to walk through the files under the tree.
 
         _log.debug('git-annex get %s (cwd=%s)', f, os.getcwd())
         assert os.path.islink(f)
@@ -144,7 +147,7 @@ class GitAnnexTool(tools.Tool):
         assert os.path.isfile(f)
 
     def drop(self, f):
-        """Drop the file from its repo"""
+        """Drop the file from its local repo."""
         _log.debug('git-annex drop %s (cwd=%s)', f, os.getcwd())
         assert os.path.islink(f)
         f, link_target = self._get_link_into_annex(f)
@@ -154,5 +157,4 @@ class GitAnnexTool(tools.Tool):
         assert os.path.islink(f)
         assert not os.path.isfile(f)
 
-
-
+# end: class GitAnnexTool
