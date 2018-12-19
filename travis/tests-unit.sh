4af4db4a7f489411e30e80c00c8e37721944b1f4
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2018-12-19 14:35:49 -0500

    re-enabled full unit tests

--- travis/tests-unit.sh
+++ travis/tests-unit.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-set -x
-
-pytest --log-cli-level=DEBUG -vvvsx --cov-append test/unit/test_tools_gcloud.py
+pytest --cov-append test/unit
 
 rc=$?; if [[ $rc != 0 ]]; then sleep 10; exit $rc; fi
 # sleep to allow logs to be printed without truncation in the event of error
