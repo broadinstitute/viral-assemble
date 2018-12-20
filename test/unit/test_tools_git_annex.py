5f04d264b701af9ea213a49e9d0b29b134465a1b
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2018-12-20 13:54:18 -0500

    git-annex testing cleanup

--- test/unit/test_tools_git_annex.py
+++ test/unit/test_tools_git_annex.py
@@ -9,6 +9,8 @@ import argparse
 import logging
 import itertools
 import uuid
+import hashlib
+import platform
 
 import pytest
 
@@ -46,78 +48,100 @@ def dir_remote(git_annex_repo, tmpdir_module):
     ga.initremote(dir_remote_name, 'directory', directory=dir_remote)
     yield dir_remote_name
 
-def test_git_annex_init_add_get_drop(git_annex_repo, dir_remote):
+def _ga_file(sfx=''):
+    file_name = 'my_file_{}_{}.txt'.format(sfx, uuid.uuid4())
+    util.file.dump_file(file_name, file_name + 'some contents')
+    ga.add(file_name)
+    ga.commit('one file')
+    return file_name
+
+@pytest.fixture
+def ga_file(git_annex_repo):
+    yield _ga_file()
+
+@pytest.fixture(scope='module')
+def file_A(git_annex_repo):
+    yield _ga_file('A')
+
+@pytest.fixture(scope='module')
+def file_B(git_annex_repo):
+    yield _ga_file('B')
+
+def _compute_md5(fname):
+    data = util.file.slurp_file(fname)
+    if platform.python_version_tuple()[0] > 2:
+        data = data.encode('utf-8')
+    return hashlib.md5(data).hexdigest().lower()
+
+def test_git_annex_init_add_get_drop(git_annex_repo, dir_remote, ga_file):
     """Test basic git-annex operations"""
 
-    file_A = 'testfile.txt'
-    util.file.dump_file(file_A, 'some contents')
-    ga.add(file_A)
-    ga.commit('one file')
-    assert isfile(file_A)
-    assert ga._get_link_into_annex(file_A)[0] == file_A
-    assert '.git/annex/objects/' in ga._get_link_into_annex(file_A)[1]
-    assert ga.lookupkey(file_A) == 'MD5E-s13--220c7810f41695d9a87d70b68ccf2aeb.txt'
+    assert isfile(ga_file)
+    ga_file_md5 = _compute_md5(ga_file)
+    ga_file_size = os.path.getsize(ga_file)
+    ga_file_key_name = ga_file_md5+os.path.splitext(ga_file)[1]
+    ga_file_key = 'MD5E-s{}--{}'.format(ga_file_size, ga_file_key_name)
+    assert ga._get_link_into_annex(ga_file)[0] == ga_file
+    assert '.git/annex/objects/' in ga._get_link_into_annex(ga_file)[1]
+    assert ga.lookupkey(ga_file) == ga_file_key
 
-    # **
+    ga.move(ga_file, to_remote_name=dir_remote)
+    assert not isfile(ga_file)
+    assert ga._get_link_into_annex(ga_file)[0] == ga_file
+    assert '.git/annex/objects/' in ga._get_link_into_annex(ga_file)[1]
+    assert ga.lookupkey(ga_file) == ga_file_key
 
-    ga.move(file_A, to_remote_name=dir_remote)
-    assert not isfile(file_A)
-    assert ga._get_link_into_annex(file_A)[0] == file_A
-    assert '.git/annex/objects/' in ga._get_link_into_annex(file_A)[1]
-    assert ga.lookupkey(file_A) == 'MD5E-s13--220c7810f41695d9a87d70b68ccf2aeb.txt'
+    ga.get(ga_file)
+    assert isfile(ga_file)
+    assert ga.is_annexed_file(ga_file)
 
-    ga.get(file_A)
-    assert isfile(file_A)
-    assert ga.is_annexed_file(file_A)
+    key_attrs = ga.examinekey(ga.lookupkey(ga_file))
+    assert key_attrs['key_name'] == ga_file_key_name
+    assert key_attrs['md5'] == ga_file_md5
+    assert key_attrs['size'] == ga_file_size
 
-    key_attrs = ga.examinekey(ga.lookupkey(file_A))
-    assert key_attrs['key_name'] == '220c7810f41695d9a87d70b68ccf2aeb.txt'
-    assert key_attrs['md5'] == '220c7810f41695d9a87d70b68ccf2aeb'
-    assert key_attrs['size'] == 13
-    assert key_attrs['size'] == os.path.getsize(file_A)
-
-    ga.drop(file_A)
-    assert not isfile(file_A)
-    assert ga.is_annexed_file(file_A)
-    assert ga.lookupkey(file_A) == 'MD5E-s13--220c7810f41695d9a87d70b68ccf2aeb.txt'
+    ga.drop(ga_file)
+    assert not isfile(ga_file)
+    assert ga.is_annexed_file(ga_file)
+    assert ga.lookupkey(ga_file) == ga_file_key
 
     # test operations when the current dir is outside the repo
-    file_A_abs = abspath(file_A)
+    ga_file_abs = abspath(ga_file)
     with pushd_popd('/'):
-        ga.get(file_A_abs)
-        assert isfile(file_A_abs)
-        ga.drop(file_A_abs)
-        assert not isfile(file_A_abs)
+        ga.get(ga_file_abs)
+        assert isfile(ga_file_abs)
+        ga.drop(ga_file_abs)
+        assert not isfile(ga_file_abs)
 
-        file_A_rel = relpath(file_A_abs)
-        ga.get(file_A_rel)
-        assert isfile(file_A_abs)
+        ga_file_rel = relpath(ga_file_abs)
+        ga.get(ga_file_rel)
+        assert isfile(ga_file_abs)
 
-        ga.get(file_A_rel)
-        assert isfile(file_A_abs)
-        ga.drop(file_A_rel)
-        assert not isfile(file_A_abs)
+        ga.get(ga_file_rel)
+        assert isfile(ga_file_abs)
+        ga.drop(ga_file_rel)
+        assert not isfile(ga_file_abs)
 
     with pushd_popd('..'):
-        file_A_link_abs = 'file_A_link_abs'
-        os.symlink(file_A_abs, file_A_link_abs)
-        assert not isfile(file_A_abs)
-        ga.get(file_A_link_abs)
-        assert isfile(file_A_abs)
-        assert isfile(file_A_link_abs)
-        ga.drop(file_A_link_abs)
-        assert not isfile(file_A_abs)
-        assert not isfile(file_A_link_abs)
+        ga_file_link_abs = 'ga_file_link_abs'
+        os.symlink(ga_file_abs, ga_file_link_abs)
+        assert not isfile(ga_file_abs)
+        ga.get(ga_file_link_abs)
+        assert isfile(ga_file_abs)
+        assert isfile(ga_file_link_abs)
+        ga.drop(ga_file_link_abs)
+        assert not isfile(ga_file_abs)
+        assert not isfile(ga_file_link_abs)
 
-        file_A_rel = relpath(file_A_abs)
-        file_A_link_rel = 'file_A_link_rel'
-        os.symlink(file_A_rel, file_A_link_rel)
-        assert not isfile(file_A_rel)
-        ga.get(file_A_link_rel)
-        assert isfile(file_A_rel)
-        assert isfile(file_A_link_rel)
-        ga.drop(file_A_link_rel)
-        assert not isfile(file_A_rel)
-        assert not isfile(file_A_link_rel)
+        ga_file_rel = relpath(ga_file_abs)
+        ga_file_link_rel = 'ga_file_link_rel'
+        os.symlink(ga_file_rel, ga_file_link_rel)
+        assert not isfile(ga_file_rel)
+        ga.get(ga_file_link_rel)
+        assert isfile(ga_file_rel)
+        assert isfile(ga_file_link_rel)
+        ga.drop(ga_file_link_rel)
+        assert not isfile(ga_file_rel)
+        assert not isfile(ga_file_link_rel)
     # end: with pushd_popd('..'):
 # end: def test_git_annex_init_add_get_drop(tmpdir_function):
