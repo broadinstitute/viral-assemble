a050467b67312af27bf9892b26ded18fad9cdfa1
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2019-02-13 13:24:55 -0500

    testing - ci - added dx login to pytest

--- travis/tests-pytest.sh
+++ travis/tests-pytest.sh
@@ -2,6 +2,10 @@
 
 set -eu -o pipefail
 
+# log in to DNAnexus
+dx login --token "$DX_API_TOKEN" --noprojects
+dx select $DX_PROJECT
+
 pytest --cov-append $PYTEST_EXTRA_OPTS test
 
 rc=$?; if [[ $rc != 0 ]]; then sleep 10; exit $rc; fi
