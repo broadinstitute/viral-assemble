0aecf323178d3bb7a5ef95535ad5b43fe921d170
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2019-02-23 13:02:29 -0500

    restoring normal travis

--- travis/tests-pytest.sh
+++ travis/tests-pytest.sh
@@ -9,7 +9,7 @@ dx pwd
 dx ls -l
 dx download file-FVQ74G80f5zf4V5vK0GjB1q5
 
-pytest --cov-append $PYTEST_EXTRA_OPTS test/unit/test_workflow_utils.py::test_starting_cromwell_server 
+pytest --cov-append $PYTEST_EXTRA_OPTS test/
 
 rc=$?; if [[ $rc != 0 ]]; then sleep 10; exit $rc; fi
 # sleep to allow logs to be printed without truncation in the event of error
