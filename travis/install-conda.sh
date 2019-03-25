64bf645573594b8dc3d65286abbe4a90a3478cee
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2019-03-25 18:43:54 -0400

    set channel_priority strict as config option

--- travis/install-conda.sh
+++ travis/install-conda.sh
@@ -41,7 +41,7 @@ else # if it does not exist, we need to install miniconda
     hash -r
     conda update -c conda-canary conda # for pre-release conda
     EXTRA_PINS="openssl=1.1.1b"
-    conda config --set always_yes yes --set changeps1 no --set remote_max_retries 6
+    conda config --set always_yes yes --set changeps1 no --set remote_max_retries 6 --set channel_priority strict
     conda config --add channels defaults
     conda config --add channels bioconda
     conda config --add channels conda-forge
@@ -49,9 +49,9 @@ else # if it does not exist, we need to install miniconda
     # Use recommendations from https://github.com/bioconda/bioconda-recipes/issues/13774
     conda update -y conda
     # conda config --set channel_priority strict
-    conda install -y --debug --strict-channel-priority pycryptosat ${EXTRA_PINS}
+    conda install -y --debug pycryptosat ${EXTRA_PINS}
     conda config --set sat_solver pycryptosat
-    conda install --quiet -y --strict-channel-priority openjdk=8.0.152 ${EXTRA_PINS}
+    conda install --quiet -y openjdk=8.0.152 ${EXTRA_PINS}
 fi
 
 # update certs
