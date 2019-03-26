23de401c073e3516439eb5c3462c5beb2513ad08
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2019-03-25 21:00:05 -0400

    remove strict channel priority

--- travis/install-conda.sh
+++ travis/install-conda.sh
@@ -41,7 +41,7 @@ else # if it does not exist, we need to install miniconda
     hash -r
     conda update -y -c conda-canary conda # for pre-release conda
     EXTRA_PINS="openssl=1.1.1b"
-    conda config --set always_yes yes --set changeps1 no --set remote_max_retries 6 --set channel_priority strict
+    conda config --set always_yes yes --set changeps1 no --set remote_max_retries 6 #--set channel_priority strict
     conda config --add channels defaults
     conda config --add channels bioconda
     conda config --add channels conda-forge
